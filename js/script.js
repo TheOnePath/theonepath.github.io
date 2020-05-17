5470c6ed9380e6b6ba60277d7adb04026ab0f151
TheOnePath <35313544+TheOnePath@users.noreply.github.com>
2020-05-17 18:56:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var msg_cliHieghts = [];
+
 function adjustBg(){
     let window_w = window.innerWidth | document.documentElement.clientWidth;
     if (window_w > 600){
@@ -22,14 +24,60 @@ function adjustBg(){
         }
     }
 }
-function a(){
-    document.querySelector('#header').remove();
+
+function viewMore(c){
+    c.classList.add('view-more');
+};
+
+function viewLess(c){
+    c.classList.add('view-less');
+};
+
+function rmViewMore(event){
+    var event = event.target;
+    if (event.nodeName !== 'P'){
+        event.setAttribute('style', '');
+        event.setAttribute('onclick', 'rmViewLess(event);');
+        event.classList.remove('view-more');
+
+        viewLess(event);
+    }
+}
+
+function rmViewLess(event){
+    var event = event.target;
+    if (event.nodeName !== 'P'){
+        event.setAttribute('style', `max-height: ${msg_cliHieghts[0]}px`);
+        event.setAttribute('onclick', 'rmViewMore(event);')
+        event.classList.remove('view-less');
+        
+        viewMore(event);
+    }
+}
+
+function a(){    
     let window_w = window.innerWidth | document.documentElement.clientWidth;
-    
     if (window_w < 601){
+        document.querySelector('#header').remove();
+
         var containers = document.querySelectorAll('.container');
         [].forEach.call(containers, function(m){
             m.classList.remove("container");
         });
+    }
+
+    var target = document.querySelectorAll('.announce-message');
+    [].forEach.call(target, function(element) {
+        msg_cliHieghts.push(element.clientHeight);
+    });
+
+    msg_cliHieghts.sort();
+    for (var i=0; i < target.length; i++){
+        if (target[i].clientHeight > msg_cliHieghts[0]) {
+            target[i].setAttribute('onclick', 'rmViewMore(event);');
+            target[i].setAttribute('style', `max-height: ${msg_cliHieghts[0]}px`);
+
+            viewMore(target[i])
+        }
     }
 };
